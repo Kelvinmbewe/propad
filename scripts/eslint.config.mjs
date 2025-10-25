cc46b0c74d63145542eee3b65c6d8240ddbe68b3
Kelvin Mbewe <Kmbewe959@gmail.com>
2025-10-25 13:30:35 +0200

    chore: adopt flat eslint and dependency updates

---
+++ scripts/eslint.config.mjs
@@ -0,0 +1,8 @@
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+import { createConfig } from '../eslint.config.mjs';
+
+const tsconfigRootDir = dirname(fileURLToPath(import.meta.url));
+
+export default createConfig(tsconfigRootDir);
